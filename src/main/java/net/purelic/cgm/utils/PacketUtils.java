6c7e833191855dcfffd253ec5c5c946d63e3dd87
WithoutRegret <mcwithoutregret@gmail.com>
2021-06-08 23:24:40 -0700

    Remove legacy code

--- src/main/java/net/purelic/cgm/utils/PacketUtils.java
+++ src/main/java/net/purelic/cgm/utils/PacketUtils.java
@@ -9,22 +9,7 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
 public class PacketUtils {
-
-    private static Method handle;
-    private static Method sendPacket;
-    private static Method center;
-    private static Method distance;
-    private static Method time;
-    private static Method movement;
-    private static Field player_connection;
-    private static Constructor<?> constructor;
-    private static Constructor<?> border_constructor;
-    private static Object constant;
 
     public static void sendPackets(Player player, Packet<?>... packets) {
         PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
@@ -58,7 +43,7 @@ public class PacketUtils {
         DataWatcher watcher = fakePlayer.getDataWatcher();
         watcher.watch(10, (byte) 127);
 
-        Packet<?>[] packets = new Packet<?>[] {
+        Packet<?>[] packets = new Packet<?>[]{
             // Spawn the fake player
             new PacketPlayOutNamedEntitySpawn(fakePlayer),
 
@@ -90,126 +75,6 @@ public class PacketUtils {
             // Send fake player/death animation packets
             sendPackets(online, packets);
         }
-    }
-
-//
-//    public static void addToTab(Player player, Participant hidden) {
-//        addToTab(player, getFakePlayer(hidden));
-//    }
-//
-//    private static void addToTab(Player player, EntityPlayer fakePlayer) {
-//        PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
-//        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, fakePlayer));
-//    }
-//
-//    public static void removeFromTab(Player player) {
-//        EntityPlayer entityPlayer = ((CraftPlayer) player).getHandle();
-//        for (Player online : Bukkit.getOnlinePlayers()) {
-//            if (!online.getUniqueId().equals(player.getUniqueId())) {
-//                PlayerConnection connection = ((CraftPlayer) online).getHandle().playerConnection;
-//                connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, entityPlayer));
-//            }
-//        }
-//    }
-//
-//    public static void removeBorder(Player player) {
-//        // sendWorldBorderPacket(player, 0);
-//    }
-
-    public static void setBorder(Player player) {
-        int percent = 100;
-        int dist = -10000 * percent + 1300000;
-        sendWorldBorderPacket(player, dist);
-    }
-
-    private static void sendWorldBorderPacket(Player player, int dist) {
-        double oldRadius = 200000.0D;
-        double newRadius = 200000.0D;
-        long delay = 0L;
-
-        try {
-            Object wb = border_constructor.newInstance();
-            center.invoke(wb, player.getLocation().getX(), player.getLocation().getY());
-            distance.invoke(wb, dist);
-            time.invoke(wb, 15);
-            movement.invoke(wb, oldRadius, newRadius, delay);
-
-            Object packet = constructor.newInstance(wb, constant);
-            sendPacket.invoke(player_connection.get(handle.invoke(player)), packet);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    static {
-        try {
-            Class<?> enumclass;
-            handle = getClass("org.bukkit.craftbukkit", "entity.CraftPlayer").getMethod("getHandle");
-            player_connection = getClass("net.minecraft.server", "EntityPlayer").getField("playerConnection");
-
-            for (Method m : getClass("net.minecraft.server", "PlayerConnection").getMethods()) {
-                if (m.getName().equals("sendPacket")) {
-                    sendPacket = m;
-
-
-                    break;
-                }
-            }
-
-            try {
-                enumclass = getClass("net.minecraft.server", "EnumWorldBorderAction");
-            } catch (ClassNotFoundException x) {
-                enumclass = getClass("net.minecraft.server", "PacketPlayOutWorldBorder$EnumWorldBorderAction");
-            }
-
-            constructor = getClass("net.minecraft.server", "PacketPlayOutWorldBorder").getConstructor(getClass("net.minecraft.server", "WorldBorder"), enumclass);
-            border_constructor = getClass("net.minecraft.server", "WorldBorder").getConstructor();
-
-            Method[] methods = getClass("net.minecraft.server", "WorldBorder").getMethods();
-
-            String setCenter = "setCenter";
-            String setWarningDistance = "setWarningDistance";
-            String setWarningTime = "setWarningTime";
-            String transitionSizeBetween = "transitionSizeBetween";
-
-            if (inClass(methods, setCenter))
-                setCenter = "c";
-            if (inClass(methods, setWarningDistance))
-                setWarningDistance = "c";
-            if (inClass(methods, setWarningTime))
-                setWarningTime = "b";
-            if (inClass(methods, transitionSizeBetween)) {
-                transitionSizeBetween = "a";
-            }
-
-            center = getClass("net.minecraft.server", "WorldBorder").getMethod(setCenter, double.class, double.class);
-            distance = getClass("net.minecraft.server", "WorldBorder").getMethod(setWarningDistance, int.class);
-            time = getClass("net.minecraft.server", "WorldBorder").getMethod(setWarningTime, int.class);
-            movement = getClass("net.minecraft.server", "WorldBorder").getMethod(transitionSizeBetween, double.class, double.class, long.class);
-
-            for (Object o : enumclass.getEnumConstants()) {
-                if (o.toString().equals("INITIALIZE")) {
-                    constant = o;
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static boolean inClass(Method[] methods, String methodName) {
-        for (Method m : methods) {
-            if (m.getName().equals(methodName)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static Class<?> getClass(String prefix, String name) throws Exception {
-        return Class.forName(prefix + "." + Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf(".") + 1) + "." + name);
     }
 
 }
