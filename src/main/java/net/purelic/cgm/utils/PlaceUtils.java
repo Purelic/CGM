8a5295b293e92610e52e6f011fefefed31e779d7
WithoutRegret <mcwithoutregret@gmail.com>
2021-08-01 16:36:49 -0700

    Clean up sending match results

--- src/main/java/net/purelic/cgm/utils/PlaceUtils.java
+++ src/main/java/net/purelic/cgm/utils/PlaceUtils.java
@@ -2,6 +2,10 @@ package net.purelic.cgm.utils;
 
 public class PlaceUtils {
 
+    public static String getPlace(int place) {
+        return place + getPlaceSuffix(place) + " Place";
+    }
+
     public static String getPlaceSuffix(int place) {
         if (place == 1 || (place % 10 == 1 && place > 20)) return "st";
         else if (place == 2 || (place % 10 == 2 && place > 20)) return "nd";
