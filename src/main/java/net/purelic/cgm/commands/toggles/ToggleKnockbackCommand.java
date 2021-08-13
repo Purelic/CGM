deb9d31783e37f710b7a8a905bcdd6ee920dd66d
WithoutRegret <mcwithoutregret@gmail.com>
2021-08-12 21:43:33 -0700

    Enable custom knockback by default

--- src/main/java/net/purelic/cgm/commands/toggles/ToggleKnockbackCommand.java
+++ src/main/java/net/purelic/cgm/commands/toggles/ToggleKnockbackCommand.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 
 public class ToggleKnockbackCommand implements CustomCommand {
 
-    public static boolean knockback = false;
+    public static boolean knockback = true;
 
     @Override
     public Command.Builder<CommandSender> getCommandBuilder(BukkitCommandManager<CommandSender> mgr) {
