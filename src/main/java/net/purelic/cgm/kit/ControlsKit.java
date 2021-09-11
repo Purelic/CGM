7ec70bae5be0a834063fc88f94ea25b011f4ce08
WithoutRegret <mcwithoutregret@gmail.com>
2021-09-11 14:20:40 -0700

    Add custom armor colors to the controls kit

--- src/main/java/net/purelic/cgm/kit/ControlsKit.java
+++ src/main/java/net/purelic/cgm/kit/ControlsKit.java
@@ -1,6 +1,10 @@
 package net.purelic.cgm.kit;
 
 import net.purelic.cgm.CGM;
+import net.purelic.commons.Commons;
+import net.purelic.commons.profile.Preference;
+import net.purelic.commons.profile.Profile;
+import net.purelic.commons.profile.preferences.ArmorColor;
 import net.purelic.commons.utils.ItemCrafter;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -12,6 +16,16 @@ public class ControlsKit implements Kit {
     @Override
     public void apply(Player player) {
         player.getInventory().addItem(this.getSetNextItem(), (CGM.getPlaylist().isUHC() ? this.getUHCItem() : this.getTogglesItem()));
+
+        ArmorColor color = ArmorColor.YELLOW;
+        Profile profile = Commons.getProfile(player);
+        String colorPref = (String) profile.getPreference(Preference.ARMOR_COLOR_UNLOCK, ArmorColor.YELLOW.name());
+
+        if (ArmorColor.contains(colorPref)) {
+            color = ArmorColor.valueOf(colorPref.toUpperCase());
+        }
+
+        player.getInventory().setArmorContents(this.getArmor(color));
     }
 
     private ItemStack getSetNextItem() {
@@ -36,6 +50,19 @@ public class ControlsKit implements Kit {
             .command("uhc", false)
             .addTag("locked")
             .craft();
+    }
+
+    private ItemStack[] getArmor(ArmorColor armorColor) {
+        return new ItemStack[]{
+            this.getColoredArmor(Material.LEATHER_BOOTS, armorColor.getBoots()),
+            this.getColoredArmor(Material.LEATHER_LEGGINGS, armorColor.getLeggings()),
+            this.getColoredArmor(Material.LEATHER_CHESTPLATE, armorColor.getChestplate()),
+            this.getColoredArmor(Material.LEATHER_HELMET, armorColor.getHelmet())
+        };
+    }
+
+    private ItemStack getColoredArmor(Material item, int rgb) {
+        return new ItemCrafter(item).color(rgb).craft();
     }
 
 }
